e4366e7e2b922cd7c2ee637c6864d26e7e5b3496
stevehiehn <stevehiehn@gmail.com>
2023-12-05 08:44:28 -0800

    updated links

--- src/.vuepress/.temp/pages/community-remotes/index.html.js
+++ src/.vuepress/.temp/pages/community-remotes/index.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-b08e8602\",\"path\":\"/community-remotes/\",\"title\":\"Resources\",\"lang\":\"en-US\",\"frontmatter\":{\"sidebar\":\"auto\"},\"headers\":[{\"level\":2,\"title\":\"Google Colabs\",\"slug\":\"google-colabs\",\"link\":\"#google-colabs\",\"children\":[]}],\"git\":{\"updatedTime\":1701651573000,\"contributors\":[{\"name\":\"stevehiehn\",\"email\":\"stevehiehn@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"community-remotes/README.md\"}")
+export const data = JSON.parse("{\"key\":\"v-b08e8602\",\"path\":\"/community-remotes/\",\"title\":\"Resources\",\"lang\":\"en-US\",\"frontmatter\":{\"sidebar\":\"auto\"},\"headers\":[{\"level\":2,\"title\":\"Google Colabs\",\"slug\":\"google-colabs\",\"link\":\"#google-colabs\",\"children\":[]}],\"git\":{\"updatedTime\":1701755995000,\"contributors\":[{\"name\":\"stevehiehn\",\"email\":\"stevehiehn@gmail.com\",\"commits\":2}]},\"filePathRelative\":\"community-remotes/README.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
