2efdff5d007abd75d3c57a53b594628bf68c1ab0
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2023-12-05 16:17:59 -0800

    remove dist

--- src/.vuepress/.temp/internal/pagesData.js
+++ src/.vuepress/.temp/internal/pagesData.js
@@ -3,16 +3,16 @@ export const pagesData = {
   "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
   // path: /client/
   "v-c55e0eaa": () => import(/* webpackChunkName: "v-c55e0eaa" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/client/index.html.js").then(({ data }) => data),
-  // path: /getting-started/
-  "v-ccdc4da0": () => import(/* webpackChunkName: "v-ccdc4da0" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/getting-started/index.html.js").then(({ data }) => data),
   // path: /community-remotes/
   "v-b08e8602": () => import(/* webpackChunkName: "v-b08e8602" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/community-remotes/index.html.js").then(({ data }) => data),
+  // path: /getting-started/
+  "v-ccdc4da0": () => import(/* webpackChunkName: "v-ccdc4da0" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/getting-started/index.html.js").then(({ data }) => data),
   // path: /plugin/
   "v-4dabea43": () => import(/* webpackChunkName: "v-4dabea43" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/plugin/index.html.js").then(({ data }) => data),
-  // path: /intro/
-  "v-f9e30908": () => import(/* webpackChunkName: "v-f9e30908" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/intro/index.html.js").then(({ data }) => data),
   // path: /remote-compute/
   "v-bdb4d734": () => import(/* webpackChunkName: "v-bdb4d734" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/remote-compute/index.html.js").then(({ data }) => data),
+  // path: /intro/
+  "v-f9e30908": () => import(/* webpackChunkName: "v-f9e30908" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/intro/index.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
 }
