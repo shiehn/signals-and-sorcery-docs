f8797c6240ab698de0a573989971e431990ee215
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2024-01-04 16:48:22 -0800

    fix download link

--- src/.vuepress/.temp/internal/pagesData.js
+++ src/.vuepress/.temp/internal/pagesData.js
@@ -5,14 +5,14 @@ export const pagesData = {
   "v-c55e0eaa": () => import(/* webpackChunkName: "v-c55e0eaa" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/client/index.html.js").then(({ data }) => data),
   // path: /community/
   "v-20f71d1f": () => import(/* webpackChunkName: "v-20f71d1f" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/community/index.html.js").then(({ data }) => data),
-  // path: /intro/
-  "v-f9e30908": () => import(/* webpackChunkName: "v-f9e30908" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/intro/index.html.js").then(({ data }) => data),
   // path: /getting-started/
   "v-ccdc4da0": () => import(/* webpackChunkName: "v-ccdc4da0" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/getting-started/index.html.js").then(({ data }) => data),
-  // path: /remotes/
-  "v-8fd2124a": () => import(/* webpackChunkName: "v-8fd2124a" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/remotes/index.html.js").then(({ data }) => data),
   // path: /plugin/
   "v-4dabea43": () => import(/* webpackChunkName: "v-4dabea43" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/plugin/index.html.js").then(({ data }) => data),
+  // path: /intro/
+  "v-f9e30908": () => import(/* webpackChunkName: "v-f9e30908" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/intro/index.html.js").then(({ data }) => data),
+  // path: /remotes/
+  "v-8fd2124a": () => import(/* webpackChunkName: "v-8fd2124a" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/remotes/index.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/stevehiehn/dawnet/dawnet-docs/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
 }
