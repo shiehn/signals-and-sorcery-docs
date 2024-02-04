b6f52143721e7fc8a29bb46d48bd9a982c1c8f4c
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2024-02-03 16:09:51 -0800

    0.6.0

--- src/.vuepress/.temp/pages/community/index.html.js
+++ src/.vuepress/.temp/pages/community/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-20f71d1f\",\"path\":\"/community/\",\"title\":\"Resources\",\"lang\":\"en-US\",\"frontmatter\":{\"sidebar\":\"auto\"},\"headers\":[{\"level\":2,\"title\":\"Source Code\",\"slug\":\"source-code\",\"link\":\"#source-code\",\"children\":[]},{\"level\":2,\"title\":\"Discussion\",\"slug\":\"discussion\",\"link\":\"#discussion\",\"children\":[]},{\"level\":2,\"title\":\"Contact\",\"slug\":\"contact\",\"link\":\"#contact\",\"children\":[]}],\"git\":{\"updatedTime\":1705652280000,\"contributors\":[{\"name\":\"Steve Hiehn\",\"email\":\"stevehiehn@Steves-Mac-mini.hitronhub.home\",\"commits\":2}]},\"filePathRelative\":\"community/README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
